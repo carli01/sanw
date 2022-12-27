20182e9ccf84a8a0265622515bcfd329dda25c67
carli01 <117832462+carli01@users.noreply.github.com>
2022-12-26 21:54:10 -0300

    Add files via upload

--- ninja.js
+++ ninja.js
@@ -16,4 +16,31 @@ class ninja{
           this.fuerza += 10;
     }
 
+  
+
 }
+
+class sensei extends ninja{
+    constructor(nombre,salud,velocidad,fuerza){
+        super(nombre,salud,velocidad,fuerza);
+        this.nombre=nombre;
+        this.salud=salud;
+        this.salud=200;
+        this.velocidad=10;
+        this.fuerza=10;
+        this.sabiduria=10;
+    }
+    speakWisdom(){
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+        super.drinkSake();
+    }
+
+    mostar_Informacion(){
+        console.log(`El ninja es ${this.nombre} su salud ${this.salud} su velocidad ${this.velocidad} su fuerza ${this.fuerza} y su sabiduria ${this.sabiduria}`)
+    }
+}
+
+var kkshi = new sensei("kakashi",)
+kkshi.mostar_Informacion();
+kkshi.speakWisdom();
+kkshi.mostar_Informacion();
